3e4a705f1cf6fdf90382e0a0b29bb8dd1637db64
thaibinhblack <thaibinhblack@gmail.com>
2019-09-14 02:00:49 +0700

    edit cors

--- .history/app/Http/Middleware/Cors_20190914015929.php
+++ .history/app/Http/Middleware/Cors_20190914015929.php
@@ -8,7 +8,7 @@ class Cors
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+        ->header('Access-Controll-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Controll-Allow-Headers', 'Content-Type', 'Authorization');
     }
 }
